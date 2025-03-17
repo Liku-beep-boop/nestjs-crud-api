a5ae083fcdbcfff41ff21621fdc1ac8b02b17e46
Liku-beep-boop <daniel.trake02@gmail.com>
2025-03-16 23:51:34 -0300

    add DELETE-endPoint

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -30,7 +30,9 @@ export class RecipeService {
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} recipe`;
+  async remove(id: number) {
+    return await this.prisma.recipe.delete({
+      where: { id },
+    });
   }
 }
